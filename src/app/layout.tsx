069131609fcb5a673e5f68480fc812e39bc6f152
programmerpranit <87561252+programmerpranit@users.noreply.github.com>
2025-05-07 08:19:37 +0000

    add ga tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "./(home)/Footer";
 import "react-toastify/dist/ReactToastify.css";
 import ToastContainerWrapper from "@/components/ToastContainerWrapper";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: "Pranit Patil - Full Stack Developer",
@@ -31,6 +32,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+ <GoogleAnalytics gaId="G-326KGTZH86" />
       </head>
       <body>
         <ToastContainerWrapper />
